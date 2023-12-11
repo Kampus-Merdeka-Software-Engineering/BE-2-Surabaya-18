0ee82169011c11cb15dea9cb2b792127c1fc2e40
harahadian <harahadian@gmail.com>
2023-12-11 14:32:16 +0700

    update

--- Routes/contactusRoutes.js
+++ Routes/contactusRoutes.js
@@ -1,6 +1,5 @@
 const express = require('express');
 const router = express.Router();
-
 const contactusController = require ('../Controller/contactusController');
 
 router.post ('/contactus', contactusController.createcontactus);
--- server.js
+++ server.js
@@ -1,15 +1,14 @@
 require('dotenv').config();
 const express = require('express');
 const cors = require('cors');
+const contactusRoutes = require("./Routes/contactusRoutes");
+const sellRoutes = require("./Routes/sellRoutes");
 
 const app = express();
 
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended : true}));
-
-const contactusRoutes = require("./Routes/contactusRoutes");
-const sellRoutes = require("./Routes/sellRoutes");
 
 app.use(contactusRoutes);
 app.use(sellRoutes);
